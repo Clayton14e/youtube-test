54232453b9ad7aaa63286599f69e8089f3e92b09
Clayton <clayton.fortini@gmail.com>
2019-12-02 23:46:56 -0500

    Working

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -5,7 +5,7 @@ import { HttpClient } from '@angular/common/http';
 @Injectable()
 export class ApiService {
   apiKey = 'AIzaSyAYLMkU9lmHJG8TMcWra6NY94k9VZNk6WY';
-  baseUrl = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q='; // Insert search term after q
+  baseUrl = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q='; // Insert search term after q
   constructor(public httpClient: HttpClient) { }
 
   getYoutube(query) {
